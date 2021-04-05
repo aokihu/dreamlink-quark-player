1e08a5a55ac7247a47104e455d7572236d29154a
aokihu <aokihu@gmail.com>
2021-04-06 00:32:25 +0800

    重构

--- main.c
+++ main.c
@@ -12,9 +12,24 @@
 
 int main(gint argc, gchar **argv)
 {
-
   // 初始化全局应用对象
-  static QP_Application *QP_Application = g_newa(QP_Application, 1);
+  QP_Application *application;
+  application = g_new(QP_Application, 1);
+
+  QP_Player *player = g_new(QP_Player, 1);
+
+  application->player = player;
+
+  application->mainLoop = g_main_loop_new(NULL, FALSE);
+  application->mainContext = g_main_loop_get_context(application->mainLoop);
+
+  // 主循环运行
+  g_main_loop_run(application->mainLoop);
+
+  // 释放资源
+  g_main_loop_unref(application->mainLoop);
+  g_free(application->mainContext);
+  g_free(application);
 
   exit(0);
 }
