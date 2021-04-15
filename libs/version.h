c2e00d5a9767ae9979ef0317b9a3f5d6389f4e31
aokihu <aokihu@gmail.com>
2021-04-15 15:36:31 +0800

    增加了版本号

--- libs/version.h
+++ libs/version.h
@@ -1,9 +1,9 @@
 #ifndef QP_H
-#define QP_H 1.1.3
+#define QP_H 1.1.4
 
 #define QP_VERSION_MAJOR 1
 #define QP_VERSION_MINOR 1
-#define QP_VERSION_PATCH 3
+#define QP_VERSION_PATCH 4
 #define QP_RELEASE_DATE "Thu Apr 15 11:52:32 CST 2021"
 
 #endif
