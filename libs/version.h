54d7699d2192efeaa0cb920993cf9435287830cf
aokihu <aokihu@gmail.com>
2021-04-16 14:57:43 +0800

    版本号更新

--- libs/version.h
+++ libs/version.h
@@ -1,9 +1,9 @@
 #ifndef QP_H
-#define QP_H 1.1.6
+#define QP_H 1.1.7
 
 #define QP_VERSION_MAJOR 1
 #define QP_VERSION_MINOR 1
-#define QP_VERSION_PATCH 6
+#define QP_VERSION_PATCH 7
 #define QP_RELEASE_DATE __DATE__ " " __TIME__
 
 #endif
