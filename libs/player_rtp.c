62bb3fba675cc9f3c99ce47cd9d32dc87b0cfc36
aokihu <aokihu@gmail.com>
2021-08-08 12:21:44 +0800

    使用单一的udpsink实现IPv6和IPv4广播

--- libs/player_rtp.c
+++ libs/player_rtp.c
@@ -16,9 +16,8 @@ void qp_player_rtpbin_pad_added_handler(GstElement *src, GstPad *pad, gpointer *
 /**
  * 生成RTP输出组件
  * 
- *                                  -> queue -> rtpbin -> udp_sink
- * opusencode -> rtpopuspay -> tee |
- *                                  -> queue -> rtpbin -> udp_sink
+ *                                 
+ * opusencode -> rtpopuspay -> rtpbin -> udp_sink
  * 
  */
 GstElement *qp_player_make_rtp_bin(QP_Player *player)
@@ -26,17 +25,12 @@ GstElement *qp_player_make_rtp_bin(QP_Player *player)
   /* 创建一个GstBin将网络输出封装起来 */
   GstElement *bin = gst_bin_new(QP_PLAYER_ELEMENT_RTPSINKBIN);
 
-  GstElement *tee = gst_element_factory_make("tee", QP_PLAYER_ELEMENT_TEE);
-  GstElement *queue = gst_element_factory_make("queue", QP_PLYAER_ELEMENT_QUEUE);
-  GstElement *queue6 = gst_element_factory_make("queue", QP_PLAYER_ELEMENT_QUEUE6);
-  GstElement *udpsink = gst_element_factory_make("udpsink", QP_PLAYER_ELEMENT_UDPSINK);
-  GstElement *udpsink6 = gst_element_factory_make("udpsink", QP_PLAYER_ELEMENT_UDPSINK6);
   GstElement *opusencode = gst_element_factory_make("opusenc", QP_PLAYER_ELEMENT_OUPSENCODE);
   GstElement *rtpopuspay = gst_element_factory_make("rtpopuspay", QP_PLAYER_ELEMENT_RTPOPUSPAY);
   GstElement *rtpbin = gst_element_factory_make("rtpbin", "rtpbin");
-  GstElement *rtpbin6 = gst_element_factory_make("rtpbin", "rtpbin6");
+  GstElement *udpsink = gst_element_factory_make("udpsink", QP_PLAYER_ELEMENT_UDPSINK);
 
-  gst_bin_add_many(GST_BIN(bin), tee, queue, queue6, opusencode, rtpopuspay, rtpbin, rtpbin6, udpsink, udpsink6, NULL);
+  gst_bin_add_many(GST_BIN(bin), opusencode, rtpopuspay, rtpbin, udpsink, NULL);
 
   /* 给Bin添加一个输入的Pad */
   GstPad *pad = gst_element_get_static_pad(opusencode, "sink");
@@ -49,51 +43,17 @@ GstElement *qp_player_make_rtp_bin(QP_Player *player)
   /* opusencode ->rtpopuspay */
   gst_element_link(opusencode, rtpopuspay);
 
-  /* rtpopuspay -> tee */
-  gst_element_link_pads(rtpopuspay, "src", tee, "sink");
-
-  /* tee -> queue */
-  GstPad *teeRequestPad = gst_element_get_request_pad(tee, "src_%u");
-  gchar *padName = gst_pad_get_name(teeRequestPad);
-  gst_element_link_pads(tee, padName, queue, "sink");
-
-  /* tee -> queue6 */
-  GstPad *teeRequestPad6 = gst_element_get_request_pad(tee, "src_%u");
-  gchar *padName6 = gst_pad_get_name(teeRequestPad6);
-  gst_element_link_pads(tee, padName6, queue6, "sink");
-
   /* queue -> rtpbin */
-  GstPad *rtpbinSinkPad = gst_element_get_request_pad(rtpbin, "send_rtp_sink_0");
+  GstPad *rtpbinSinkPad = gst_element_get_request_pad(rtpbin, "send_rtp_sink_%u");
   gchar *rtpbinSinkPadName = gst_pad_get_name(rtpbinSinkPad);
-  gst_element_link_pads(queue, "src", rtpbin, rtpbinSinkPadName);
-
-  /* queue6 -> rtpbin6 */
-  GstPad *rtpbin6SinkPad = gst_element_get_request_pad(rtpbin6, "send_rtp_sink_0");
-  gchar *rtpbin6SinkPadName = gst_pad_get_name(rtpbin6SinkPad);
-  gst_element_link_pads(queue6, "src", rtpbin6, rtpbin6SinkPadName);
+  gst_element_link_pads(rtpopuspay, "src", rtpbin, rtpbinSinkPadName);
 
   /* rtpbin -> udpsink */
   gst_element_link_pads(rtpbin, "send_rtp_src_0", udpsink, "sink");
 
-  /* rtpbin6 -> udpsink6 */
-  gst_element_link_pads(rtpbin6, "send_rtp_src_0", udpsink6, "sink");
-
   /* 设置UDP广播参数 */
-  g_object_set(udpsink,
-               "host", player->opt_address->str,
-               "port", player->opt_port,
-               NULL);
-
-  g_object_set(udpsink6,
-               "host", player->opt_address6->str,
-               "port", player->opt_port,
-               NULL);
-
-  /* 释放资源 */
-  gst_object_unref(teeRequestPad);
-  gst_object_unref(teeRequestPad6);
-  gst_object_unref(rtpbinSinkPad);
-  gst_object_unref(rtpbin6SinkPad);
+  g_signal_emit_by_name(udpsink, "add", player->opt_address->str, player->opt_port);
+  g_signal_emit_by_name(udpsink, "add", player->opt_address6->str, player->opt_port);
 
   // gst_debug_bin_to_dot_file(bin, GST_DEBUG_GRAPH_SHOW_ALL, "kankan");
   return bin;
