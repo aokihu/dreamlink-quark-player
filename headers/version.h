786ceac1a143d5d58f25d07f9302177ae9e7606f
aokihu <aokihu@gmail.com>
2021-11-17 16:50:03 +0800

    删除decodebin的cap参数

--- headers/version.h
+++ headers/version.h
@@ -1,9 +1,9 @@
 #ifndef QP_H
-#define QP_H 2.1.1
+#define QP_H 2.1.2
 
 #define QP_VERSION_MAJOR 2
 #define QP_VERSION_MINOR 1
-#define QP_VERSION_PATCH 1
+#define QP_VERSION_PATCH 2
 #define QP_RELEASE_DATE __DATE__ " " __TIME__
 
 #endif
--- libs/player_private.c
+++ libs/player_private.c
@@ -25,7 +25,7 @@ void qp_player_make_pipeline(QP_Player *player)
       g_string_append_printf(pipeline_string, 
       "fdsrc !"
       "queue max-size-bytes=102400 min-threshold-bytes=2048 !"
-      "decodebin sink-caps=\"application/ogg\"");
+      "decodebin");
     break;
     /* 输入源是UDP */
     case QP_SET_INPUT_TYPE_UDP:
