2cb2912e4951f305e1b2f87bf317fdd22385f1d3
aokihu <aokihu@gmail.com>
2022-03-07 10:18:33 +0800

    更新版本号

--- headers/version.h
+++ headers/version.h
@@ -1,9 +1,9 @@
 #ifndef QP_H
-#define QP_H 2.1.2
+#define QP_H 2.1.3
 
 #define QP_VERSION_MAJOR 2
 #define QP_VERSION_MINOR 1
-#define QP_VERSION_PATCH 2
+#define QP_VERSION_PATCH 3
 #define QP_RELEASE_DATE __DATE__ " " __TIME__
 
 #endif
