5de9c40b77821eb84bb28a49d5b28be57416a2a0
aokihu <aokihu@gmail.com>
2023-10-02 22:37:27 +0800

    增加自己的工具库函数

---
+++ headers/utils.h
@@ -0,0 +1,12 @@
+/**
+ * 自定义工具库
+ * @brief 由于GLib库中的工具函数不够全面，所以自己写了一些工具函数
+ */
+
+/**
+ * @brief 字符串比较函数
+ * @param a { gchar* }a
+ * @param b { gchar* }b
+ * @return {gboolean} 如果相等返回 1，否则返回 0
+ */
+extern gboolean qp_utils_strequal(const gchar *a, const gchar *b);
---
+++ libs/util.c
@@ -0,0 +1,14 @@
+#include <glib.h>
+#include "../headers/utils.h"
+
+/**
+ * @brief 字符串比较函数
+ * @param a { gchar* }a
+ * @param b { gchar* }b
+ * @return {gboolean} 如果相等返回 1，否则返回 0
+ */
+
+extern gboolean qp_utils_strequal(const gchar *a, const gchar *b)
+{
+  return !strcmp((gchar *)a, (gchar *)b) ? 1 : 0;
+}
