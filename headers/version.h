7bd280d1a5f90c6e7ec209ba063ee1a215d82569
aokihu <aokihu@gmail.com>
2023-10-02 13:42:51 +0800

    Update version.h
    更新版本信息到3.0.0

--- headers/version.h
+++ headers/version.h
@@ -1,9 +1,9 @@
 #ifndef QP_H
-#define QP_H 2.2.3
+#define QP_H 3.0.0
 
-#define QP_VERSION_MAJOR 2
-#define QP_VERSION_MINOR 2
-#define QP_VERSION_PATCH 3
+#define QP_VERSION_MAJOR 3
+#define QP_VERSION_MINOR 0
+#define QP_VERSION_PATCH 0
 #define QP_RELEASE_DATE __DATE__ " " __TIME__
 
 #endif
